441c9ae066d2f3a90eb196966f3c63b7feaebc24
asekito <dresekito@gmail.com>
2020-12-08 13:40:19 -0700

    hashed passwords were being cut in the db causing login errors

--- backend/migrations/sqls/20201208035619-users-up.sql
+++ backend/migrations/sqls/20201208035619-users-up.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS users (
   userName VARCHAR(50) NOT NULL,
   firstName VARCHAR(50) NOT NULL,
   lastName VARCHAR(50) NOT NULL,
-  password VARCHAR(50) NOT NULL
+  password VARCHAR(255) NOT NULL
 );
---
+++ backend/routes/login-route.js
@@ -0,0 +1,25 @@
+const { app, User, sequelize, bcrypt, jwt } = require("../server");
+
+app.post("/api/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ where: { userName: username } });
+  // check if user found and then check if password was valid with bcrypt
+
+  if (user) {
+    const validPassword = bcrypt.compareSync(password, user.password);
+    if (!validPassword) {
+      return res
+        .status(401)
+        .send({ auth: false, token: null, error: "Incorrect password." });
+    }
+
+    const accessToken = jwt.sign(
+      { username: user.username },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: 21600 } // expires in 6 hours
+    );
+    return res.send({ auth: true, token: accessToken });
+  } else {
+    return res.status(401).send({ auth: false, error: ["User not found"] });
+  }
+});
