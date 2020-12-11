c5c3b8accd4b2d84fe4106dc0a7c7d660347a055
asekito <dresekito@gmail.com>
2020-12-10 21:08:20 -0700

    setting up with dummy data

---
+++ client/src/components/ReadingProfile/DummyData.ts
@@ -0,0 +1,37 @@
+export const dummyDataBookHistory = [
+  {
+    Title: "The Great Gatsby",
+    Author: "F. Scott Fitzgerald",
+    Rating: "N/A", // -1
+    Status: "Abandoned",
+    Reads: 0,
+  },
+  {
+    Title: "Minor Feelings",
+    Author: "Cathy Diep",
+    Rating: "9.6",
+    Status: "Completed",
+    Reads: 1,
+  },
+  {
+    Title: "The Odyssey",
+    Author: "Homer",
+    Rating: "8.2",
+    Status: "In-Progress (Re-read)",
+    Reads: 2,
+  },
+  {
+    Title: "The Handmaid's Tale",
+    Author: "Margaret Atwood",
+    Rating: "N/A", // -1 in database
+    Status: "Abandoned",
+    Reads: 0,
+  },
+];
+
+export const dummyDataBookWishList = [
+  { Title: "Ornamentalism", Author: "Anne Anlin Cheng" },
+  { Title: "2666", Author: "Roberto Bolaño" },
+  { Title: "All About Love", Author: "Bell Hooks" },
+  { Title: "Desert Solitaire", Author: "Edward Abbey" },
+];
