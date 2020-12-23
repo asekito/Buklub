bd12dfb2f35e2784e9a6fea309f3561e77f6b754
asekito <dresekito@gmail.com>
2020-12-23 12:59:01 -0700

    formatting of data and sending to client

--- backend/routes/ReadProfile/get-books-history.js
+++ backend/routes/ReadProfile/get-books-history.js
@@ -32,14 +32,43 @@ app.get("/api/literary-history", async (req, res) => {
       },
       attributes: ["id", "userName"],
       include: [{ model: Book }],
+      order: [[Book, "title", "asc"]],
     });
 
     if (!litHistory.length) {
       return res.status(201).send({ response: false, body: [] });
     }
+    const formattedBody = formatBookHistoryObject(litHistory);
 
-    return res.send({ response: true, body: litHistory });
+    return res.send({ response: true, body: formattedBody });
   } catch (err) {
     return res.status(400).send({ error: err });
   }
 });
+
+const formatBookHistoryObject = (bookHistory) => {
+  return bookHistory.map((b) => {
+    return {
+      userID: b.id,
+      userName: b.userName,
+      bookID: b["books.id"],
+      googleBookID: b["books.googleBookID"],
+      title: b["books.title"],
+      authors: b["books.authors"],
+      pageCount: b["books.pageCount"],
+      description: b["books.description"],
+      publisher: b["books.publisher"],
+      publishedDate: b["books.publishedDate"],
+      smallThumbnail: b["books.smallThumbnail"],
+      thumbnail: b["books.thumbnail"],
+      bookDetailID: b["books.userBookDetails.id"],
+      bookDetailBookRating: b["books.userBookDetails.bookRating"],
+      bookDetailBookStatus: b["books.userBookDetails.status"],
+      bookDetailBookFavorite: b["books.userBookDetails.favorite"],
+      bookDetailBookTimesRead: b["books.userBookDetails.timesRead"],
+      bookDetailBookNotes: b["books.userBookDetails.notes"],
+      bookDetailBookStartDate: b["books.userBookDetails.startDate"],
+      bookDetailBookEndDate: b["books.userBookDetails.endDate"],
+    };
+  });
+};
