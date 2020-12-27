5fe10b7949aebc96ed1e942b8df4b5dac3333291
asekito <dresekito@gmail.com>
2020-12-27 16:12:12 -0700

    modal for adding to lit history

--- client/src/components/ReadingProfile/AddToLiteraryHistory.tsx
+++ client/src/components/ReadingProfile/AddToLiteraryHistory.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import fetchCommand from "../../../utils/fetching";
 // import { TextField } from "@material-ui/core";
 // import { Autocomplete } from "@material-ui/lab";
+import "../../assets/AddToLiteraryHistory.scss";
 
 const AddToLiteraryHistory: React.FC<Props> = ({
   addToHistory,
@@ -97,17 +98,19 @@ const AddToLiteraryHistory: React.FC<Props> = ({
   }, [bookSearch]);
 
   return (
-    <div>
+    <div className="modal add-book-hx">
       <h1>Add a book</h1>
       <form id="book-add">
-        <label htmlFor="book">Book</label>
-        <input
-          name="book"
-          onChange={(e) => bookSearchHandler(e)}
-          autoComplete="off"
-          id="add-search"
-        />
-        <div>
+        <div className="book-search">
+          <label htmlFor="book">Book</label>
+          <input
+            name="book"
+            onChange={(e) => bookSearchHandler(e)}
+            autoComplete="off"
+            id="add-search"
+          />
+        </div>
+        <div className="potential-books add-book-hx">
           {potentialBooks.length > 0 ? (
             <ul>
               {potentialBooks.map((b, i) => (
@@ -132,104 +135,110 @@ const AddToLiteraryHistory: React.FC<Props> = ({
           ) : null}
         </div>
 
-        <label htmlFor="status">Status</label>
-        <select
-          name="status"
-          value={literaryHistoryBook.status}
-          onChange={(e) => changeHandler(e)}
-        >
-          <option value="0">In-Progress</option>
-          <option value="1">Completed</option>
-          <option value="2">Abandoned</option>
-          <option value="3">On Hold</option>
-        </select>
+        <div className="add-book-hx status">
+          <label htmlFor="status">Status</label>
+          <select
+            name="status"
+            value={literaryHistoryBook.status}
+            onChange={(e) => changeHandler(e)}
+          >
+            <option value="0">In-Progress</option>
+            <option value="1">Completed</option>
+            <option value="2">Abandoned</option>
+            <option value="3">On Hold</option>
+          </select>
+        </div>
 
-        <p>Rating</p>
-        <label htmlFor="rating">0</label>
-        <input
-          type="radio"
-          name="rating"
-          value="0"
-          onChange={(e) => changeHandler(e)}
-        />
-        <label htmlFor="rating">1</label>
-        <input
-          type="radio"
-          name="rating"
-          value="1"
-          onChange={(e) => changeHandler(e)}
-        />
-        <label htmlFor="rating">2</label>
-        <input
-          type="radio"
-          name="rating"
-          value="2"
-          onChange={(e) => changeHandler(e)}
-        />
-        <label htmlFor="rating">3</label>
-        <input
-          type="radio"
-          name="rating"
-          value="3"
-          onChange={(e) => changeHandler(e)}
-        />
-        <label htmlFor="rating">4</label>
-        <input
-          type="radio"
-          name="rating"
-          value="4"
-          onChange={(e) => changeHandler(e)}
-        />
-        <label htmlFor="rating">5</label>
-        <input
-          type="radio"
-          name="rating"
-          value="5"
-          onChange={(e) => changeHandler(e)}
-        />
-        <label htmlFor="rating">6</label>
-        <input
-          type="radio"
-          name="rating"
-          value="6"
-          onChange={(e) => changeHandler(e)}
-        />
-        <label htmlFor="rating">7</label>
-        <input
-          type="radio"
-          name="rating"
-          value="7"
-          onChange={(e) => changeHandler(e)}
-        />
-        <label htmlFor="rating">8</label>
-        <input
-          type="radio"
-          name="rating"
-          value="8"
-          onChange={(e) => changeHandler(e)}
-        />
-        <label htmlFor="rating">9</label>
-        <input
-          type="radio"
-          name="rating"
-          value="9"
-          onChange={(e) => changeHandler(e)}
-        />
-        <label htmlFor="rating">10</label>
-        <input
-          type="radio"
-          name="rating"
-          value="10"
-          onChange={(e) => changeHandler(e)}
-        />
+        <div className="add-book-hx rating">
+          <p>Rating</p>
+          <label htmlFor="rating">0</label>
+          <input
+            type="radio"
+            name="rating"
+            value="0"
+            onChange={(e) => changeHandler(e)}
+          />
+          <label htmlFor="rating">1</label>
+          <input
+            type="radio"
+            name="rating"
+            value="1"
+            onChange={(e) => changeHandler(e)}
+          />
+          <label htmlFor="rating">2</label>
+          <input
+            type="radio"
+            name="rating"
+            value="2"
+            onChange={(e) => changeHandler(e)}
+          />
+          <label htmlFor="rating">3</label>
+          <input
+            type="radio"
+            name="rating"
+            value="3"
+            onChange={(e) => changeHandler(e)}
+          />
+          <label htmlFor="rating">4</label>
+          <input
+            type="radio"
+            name="rating"
+            value="4"
+            onChange={(e) => changeHandler(e)}
+          />
+          <label htmlFor="rating">5</label>
+          <input
+            type="radio"
+            name="rating"
+            value="5"
+            onChange={(e) => changeHandler(e)}
+          />
+          <label htmlFor="rating">6</label>
+          <input
+            type="radio"
+            name="rating"
+            value="6"
+            onChange={(e) => changeHandler(e)}
+          />
+          <label htmlFor="rating">7</label>
+          <input
+            type="radio"
+            name="rating"
+            value="7"
+            onChange={(e) => changeHandler(e)}
+          />
+          <label htmlFor="rating">8</label>
+          <input
+            type="radio"
+            name="rating"
+            value="8"
+            onChange={(e) => changeHandler(e)}
+          />
+          <label htmlFor="rating">9</label>
+          <input
+            type="radio"
+            name="rating"
+            value="9"
+            onChange={(e) => changeHandler(e)}
+          />
+          <label htmlFor="rating">10</label>
+          <input
+            type="radio"
+            name="rating"
+            value="10"
+            onChange={(e) => changeHandler(e)}
+          />
+        </div>
 
-        <label htmlFor="timesRead">Times read</label>
-        <input
-          type="number"
-          name="timesRead"
-          autoComplete="off"
-          onChange={(e) => changeHandler(e)}
-        />
+        <div>
+          <label htmlFor="timesRead">Times read</label>
+          <input
+            type="number"
+            name="timesRead"
+            autoComplete="off"
+            onChange={(e) => changeHandler(e)}
+          />
+        </div>
 
         <label htmlFor="favorite">One of your favorites?</label>
         <select name="favorite" onChange={(e) => changeHandler(e)}>
@@ -247,21 +256,28 @@ const AddToLiteraryHistory: React.FC<Props> = ({
             onChange={(e) => changeHandler(e)}
           />
         </div>
-        <label htmlFor="startDate">Start Date</label>
-        <input
-          type="date"
-          name="startDate"
-          onChange={(e) => changeHandler(e)}
-        />
 
-        <label htmlFor="endDate">End Date</label>
-        <input type="date" name="endDate" onChange={(e) => changeHandler(e)} />
+        <div className="add-book-hx date-timeline">
+          <label htmlFor="startDate">Start Date</label>
+          <input
+            type="date"
+            name="startDate"
+            onChange={(e) => changeHandler(e)}
+          />
 
-        <input
-          type="submit"
-          value="Add Book"
-          onClick={(e) => submitHandler(e)}
-        />
+          <label htmlFor="endDate">End Date</label>
+          <input
+            type="date"
+            name="endDate"
+            onChange={(e) => changeHandler(e)}
+          />
+
+          <input
+            type="submit"
+            value="Add Book"
+            onClick={(e) => submitHandler(e)}
+          />
+        </div>
       </form>
     </div>
   );
