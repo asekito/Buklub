7ec4cbc307ba0bd8e6632be1706d3ebf49d149b3
asekito <dresekito@gmail.com>
2021-01-04 21:50:01 -0700

    simple backend to delete user book details

--- backend/routes/ReadProfile/delete-book-history.js
+++ backend/routes/ReadProfile/delete-book-history.js
@@ -7,10 +7,30 @@ const {
   UserBookDetail,
 } = require("../../server.js");
 
-app.delete("/api/literary-history/:bookID", (req, res) => {
-  console.log(req);
-  console.log(req.params.bookID);
-  const { auth } = req.headers;
-  console.log(auth);
-  res.status(200).send({ response: true });
+app.delete("/api/literary-history/:bookID", async (req, res) => {
+  try {
+    const { auth } = req.headers;
+    const { bookID } = req.params;
+
+    if (!auth) {
+      throw new Error("Authorization denied for deletion.");
+    } else {
+      jwt.verify(auth, process.env.ACCESS_TOKEN_SECRET);
+    }
+
+    const decodedToken = jwt.decode(auth, process.env.ACCESS_TOKEN_SECRET);
+
+    // check if user exists decodedToken.user (the id)
+
+    await UserBookDetail.destroy({
+      where: {
+        userID: decodedToken.user,
+        bookID: bookID,
+      },
+    });
+
+    return res.status(200).send({ response: true });
+  } catch (err) {
+    res.status(400).send({ response: false, error: err.toString() });
+  }
 });
